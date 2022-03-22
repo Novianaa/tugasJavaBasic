33fa1619e6b29048823c842eafd197a05f9fe3ee
Novianaa <noviana.rahmawati80@gmail.com>
2022-03-22 15:40:36 +0700

    Mini Camp

---
+++ src/biodata.java
@@ -0,0 +1,18 @@
+public class biodata{
+  public static void main(String[] args) {
+    String firstName, lastName, birthPlace, progrLangFav;
+    int birthYear;
+
+    firstName="Noviana";
+    lastName="Rahmawati";
+    birthPlace="Solo";
+    progrLangFav="Javascript";
+    birthYear=2000;
+
+    System.out.println("BIODATA");
+    System.out.println("Nama : "+firstName+" "+lastName);
+    System.out.println("Tempat Lahir : "+birthPlace);
+    System.out.println("Tahun Lahir : "+birthYear);
+    System.out.println("Bahasa Pemrograman : "+progrLangFav);
+  }
+}
---
+++ src/kelipatan.java
@@ -0,0 +1,11 @@
+public class kelipatan{
+  public static void main(String[] args) {
+    int i;
+
+    for(i=1;i<=100;i++){
+      if((i%3==0) && (i%5==0)){
+        System.out.println(i);
+      }
+    }
+  }
+}
